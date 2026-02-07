4dfcaaece5d8aacd05d7d7f659575e511c5c4bb6
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-02-07 22:48:48 +0100

    first commit

---
+++ CheckIfANumberIsPositiveNegativeOrZero.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let value: i32 = -5;
+
+    if value > 0 {
+        println!("Positive number");
+    } else if value < 0 {
+        println!("Negative number");
+    } else {
+        println!("Zero");
+    }
+}
