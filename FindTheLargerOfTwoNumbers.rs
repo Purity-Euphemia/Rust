8a690083702de997f17d8c48f942fb983a6cba71
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-02-07 22:54:21 +0100

    first commit

---
+++ FindTheLargerOfTwoNumbers.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let first_number: i32 = 15;
+    let second_number: i32 = 20;
+
+    if first_number > second_number {
+        println!("First number is larger");
+    } else {
+        println!("Second number is larger");
+    }
+}
