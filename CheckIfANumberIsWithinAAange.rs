ce5c69bebe0521273379891170d028fbdca995c7
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-02-09 22:00:42 +0100

    first commit

---
+++ CheckIfANumberIsWithinAAange.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number: i32 = 15;
+
+    if number >= 10 && number <= 20 {
+        println!("Number is within range");
+    } else {
+        println!("Number is out of range");
+    }
+}
