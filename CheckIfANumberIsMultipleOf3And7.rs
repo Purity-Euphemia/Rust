4881db26368d62807776c4f26475fc6730e05800
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-02-10 03:16:53 +0100

    first commit

---
+++ CheckIfANumberIsMultipleOf3And7.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number: i32 = 21;
+
+    if number % 3 == 0 && number % 7 == 0 {
+        println!("Multiple of 3 and 7");
+    } else {
+        println!("Not a multiple of 3 and 7");
+    }
+}
