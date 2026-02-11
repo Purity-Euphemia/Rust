3b6c6787e0ed2e6b5518fa76cd2b77df1f5e5f62
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-02-11 03:51:27 +0100

    first commit

---
+++ CheckLoginAccess.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let is_username_correct: bool = true;
+    let is_password_correct: bool = false;
+
+    if is_username_correct && is_password_correct {
+        println!("Login successful");
+    } else {
+        println!("Login failed");
+    }
+}
